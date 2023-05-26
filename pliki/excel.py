3ded75b225494be8103bd9dd7f870153efac74af
Konrad Gawda <k.gawda@alx.pl>
2023-05-26 15:01:44 +0200

    jeszcze więcej excel files

--- pliki/excel.py
+++ pliki/excel.py
@@ -6,8 +6,9 @@ def main():
     ws = workbook.active
     cell = ws.cell(row=1, column=1)
     cell.value = "Hello World"
-    cell.font = openpyxl.styles.Font(color="EE5555")
-    # cell.fill = openpyxl.styles.PatternFill("solid", fgColor="DDDDDD")  # do sprawdzenia
+    cell.font = openpyxl.styles.Font(color="EE5555", bold=True)
+    cell.alignment = openpyxl.styles.Alignment(horizontal="center")
+    #cell.fill = openpyxl.styles.PatternFill(bgColor="FFC7CE", fill_type="solid")  # do sprawdzenia
 
     ws["B2"].value = 5
     ws["B2"].comment = openpyxl.comments.Comment("To jest komentarz", "kurs")
@@ -20,5 +21,13 @@ def main():
     workbook.save("test.xlsx")
     workbook.close()
 
+def load_example():
+    wb = openpyxl.load_workbook(r"C:\Users\kurs\Downloads\dane\excel_example.xlsx")
+    ws = wb.active
+    for row in ws.iter_rows(min_row=2):
+        print(row[0].value, row[4].value)  # tu wyjątkowo w openpyxl indeksowanie od zera!
+
+
 if __name__ == "__main__":
-    main()
+    load_example()
+    # main()
