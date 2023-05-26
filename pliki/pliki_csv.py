ba303fa2cf81f6feef44fd0174e44312da622187
Konrad Gawda <k.gawda@alx.pl>
2023-05-26 11:13:05 +0200

    CSV Dict reader

--- pliki/pliki_csv.py
+++ pliki/pliki_csv.py
@@ -8,7 +8,14 @@ def csv_reader(path):
             row = list(map(str.rstrip, row))
             print(row)
 
+def csv_dict_reader(path):
+    with open(path, newline="") as f:
+        reader = csv.DictReader(f, delimiter=";")
+        for row in reader:
+            row = {...}
+            print(row)
 
 if __name__ == "__main__":
     path = r"C:\Users\kurs\Downloads\dane\csv_example1.csv"
     csv_reader(path)
+    csv_dict_reader(path)
