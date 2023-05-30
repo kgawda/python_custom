15abce7a96f7126c53efe60c23f004f9391dd11e
Konrad Gawda <k.gawda@alx.pl>
2023-05-30 16:08:05 +0200

    Flask with templates

--- webapp/hello.py
+++ webapp/hello.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    # files = pathlib.Path('..').iterdir()
-    # s = "\n".join(f"<p>{f}</p>" for f in files)
-    return render_template("index.html.j2", title="Hello World!")
+    files = pathlib.Path('..').iterdir()
+    return render_template("index.html.j2", title="Hello World!", files=files)
 
 if __name__ == '__main__':
     app.run(debug=True)
