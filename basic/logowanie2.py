7928558b7fdc3a42a4bb6b014d2471aaf0c7c000
Konrad Gawda <k.gawda@alx.pl>
2023-05-30 15:03:32 +0200

    Logowanie syslog

--- basic/logowanie2.py
+++ basic/logowanie2.py
@@ -1,4 +1,6 @@
 import logging
+import logging.handlers
+
 logger = logging.getLogger("alfa")
 logger.setLevel(logging.DEBUG)
 
@@ -12,8 +14,13 @@ file_handler = logging.FileHandler("example.log")
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 
+syslog_handler = logging.handlers.SysLogHandler(('52.50.100.255', 5014))
+syslog_handler.setLevel(logging.DEBUG)
+syslog_handler.setFormatter(formatter)
+
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
+logger.addHandler(syslog_handler)
 
 import importowany2
 
