aefd06e8e689861198f28069181ea0f6e456bd7b
Konrad Gawda <k.gawda@alx.pl>
2023-05-30 11:46:05 +0200

    Soap

---
+++ internet/polaczenia_soap.py
@@ -0,0 +1,11 @@
+import zeep
+
+def main():
+    url = "http://www.dneonline.com/calculator.asmx?WSDL"
+    soap = zeep.Client(url, service_name="Calculator", port_name="CalculatorSoap")
+    print(soap.service.Add(1, 3))
+    print(soap.service.Divide(1234, 100))
+
+
+if __name__ == "__main__":
+    main()
