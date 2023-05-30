acc9011efdc52315da523dfba136d639c2e06727
Konrad Gawda <k.gawda@alx.pl>
2023-05-30 11:06:12 +0200

    SSH dodatkowe przykłady

--- internet/ssh.py
+++ internet/ssh.py
@@ -1,20 +1,25 @@
 from fabric import Connection
 
+def example_parsing(s):
+    files = []
+    for line in s.splitlines():
+        if not line.startswith("total"):
+            files.append(line.split()[-1])
+    print("Files to process:", ", ".join(files))
+
 def main():
     with Connection("52.50.100.255", user="kurs", connect_kwargs={"password": ""}) as conn:
+        with conn.cd("/tmp"):  # sprytny context manager
+            conn.run("ls -al")
         # conn.put(), conn.get()  # proste operacje na plikach
-        conn.run("touch Konrad")
+        conn.run("touch Konrad")  # polecenie touch tworzy pusty plik o zadanej nazwie
         result = conn.run("ls -al")
 
     # przykład obróbki wyniku tekstowego z result.stdout
     if result.exited == 0:
-        files = []
-        for line in result.stdout.splitlines():
-            if not line.startswith("total"):
-                files.append(line.split()[-1])
-        print("Files to process:", ", ".join(files))
-
+        example_parsing(result.stdout)
         # result.stderr  # informacje o błędach
+
 
 if __name__ == "__main__":
     main()
