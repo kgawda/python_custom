62f6dcdf46abe4312f9a3272e2ff4d7009ba584b
Konrad Gawda <k.gawda@alx.pl>
2023-05-31 13:09:53 +0200

    MSSQL

--- bazy_danych/test_mssql.py
+++ bazy_danych/test_mssql.py
@@ -2,17 +2,24 @@ from pymssql import _mssql
 
 def main(server, user, password):
     with _mssql.connect(server=server, user=user, password=password, database="tempdb") as conn:
-        conn.execute_query('SELECT name FROM master.sys.databases')
+
+        # wiele wierszy
+        # conn.execute_query('SELECT name FROM master.sys.databases')
+        conn.execute_query('SELECT * FROM empl WHERE id=%d', 13)
         for row in conn:
-          print(row)
+            print(row)
+
+        # jedne wiersz
+        employeedata = conn.execute_row("SELECT * FROM employees WHERE id=%d", 13)
+
+        # jedna liczba
+        numemployees = conn.execute_scalar("SELECT COUNT(*) FROM employees")
+
+        # nic nie zwraca
+        conn.execute_non_query('CREATE TABLE persons(id INT, name VARCHAR(100))')
 
 if __name__ == "__main__":
     try:
-        main(server="localhost", user="sa", password="")
+        main(server="52.51.0.112", user="sa", password="...")
     except _mssql.MSSQLDatabaseException as e:
-        print("Nie działa")
-
-    try:
-        main(server="...", user="sa", password="...")
-    except _mssql.MSSQLDatabaseException as e:
-        print("Nie działa")
+        print("Błąd połączenia")
