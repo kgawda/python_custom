8b1637adeb0a92271f626223a09b3bf57273dd49
Konrad Gawda <k.gawda@alx.pl>
2023-05-31 13:20:15 +0200

    MSSQL inaczej

--- bazy_danych/test_mssql.py
+++ bazy_danych/test_mssql.py
@@ -1,4 +1,6 @@
-from pymssql import _mssql
+from pymssql import _mssql, connect
+import pandas as pd
+import pyodbc
 
 def main(server, user, password):
     with _mssql.connect(server=server, user=user, password=password, database="tempdb") as conn:
@@ -18,8 +20,25 @@ def main(server, user, password):
         # nic nie zwraca
         conn.execute_non_query('CREATE TABLE persons(id INT, name VARCHAR(100))')
 
+
+def main_cursor(server, user, password):
+    with connect(server=server, user=user, password=password, database="tempdb") as conn:
+        with conn.cursor(as_dict=True) as cursor:
+            cursor.execute('SELECT name FROM master.sys.databases')
+            for row in cursor:
+                print(row)
+
+def main_pandas():
+    conn = pyodbc.connect('Driver={SQL Server};'  # to może być np. {SQL Server Native Client 11.0}
+                          'Server=ServerName;'
+                          'Database=DatabaseName;'
+                          'Trusted_Connection=yes;')
+    df = pd.read_sql_query('SELECT * FROM products', conn)
+    print(df)
+
 if __name__ == "__main__":
     try:
         main(server="52.51.0.112", user="sa", password="...")
+        main_cursor(server="52.51.0.112", user="sa", password="...")
     except _mssql.MSSQLDatabaseException as e:
         print("Błąd połączenia")
